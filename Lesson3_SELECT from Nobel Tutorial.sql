aeb80483d7c70a5bcf1e166374cc8253770acf97
JoyceLee16 <stillsmile016@gmail.com>
2017-06-02 00:04:00 +0800

    Update Lesson3_SELECT from Nobel Tutorial.sql

--- Lesson3_SELECT from Nobel Tutorial.sql
+++ Lesson3_SELECT from Nobel Tutorial.sql
@@ -1,12 +1,116 @@
 --#1
 /*
-Show the name, continent and population of all countries.
+Change the query shown so that it displays Nobel prizes for 1950.
 */
-SELECT name, continent, population FROM world
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 
 --#2
 /*
-Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+Show who won the 1962 prize for Literature
 */
-SELECT name FROM world
-WHERE population >= 200000000
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+   
+--#3
+/*
+Show the year and subject that won 'Albert Einstein' his prize.
+*/
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein'
+
+--#4
+/*
+Give the name of the 'Peace' winners since the year 2000, including 2000.
+*/
+SELECT winner
+  FROM nobel
+ WHERE subject = 'Peace'
+   AND yr >= 2000
+
+--#5
+/*
+Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+*/
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE subject = 'Literature' 
+   AND yr BETWEEN 1980 AND 1989;
+
+--#6
+/*
+Show all details of the presidential winners:
+Theodore Roosevelt
+Woodrow Wilson
+Jimmy Carter
+Barack Obama
+*/
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 
+                   'Woodrow Wilson', 
+                   'Jimmy Carter', 
+                   'Barack Obama')     
+
+--#7
+/*
+Show the winners with first name John
+*/
+SELECT winner FROM nobel
+ WHERE LEFT(winner,4) = 'John'  
+
+--#8
+/*
+Show the Physics winners for 1980 together with the Chemistry winners for 1984.
+*/
+SELECT * FROM nobel
+ WHERE (yr = 1980 AND subject = 'Physics')
+  OR   (yr = 1984 AND subject = 'Chemistry')     
+   
+--#9
+/*
+Show the winners for 1980 excluding the Chemistry and Medicine
+*/
+SELECT * FROM nobel
+ WHERE subject NOT IN ('Medicine', 'Chemistry')
+  AND yr = 1980;
+
+--#10
+/*
+Show who won a 'Medicine' prize in an early year (before 1910, not including 1910) together 
+with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+*/
+SELECT * FROM nobel
+ WHERE (subject = 'Medicine' AND yr < 1910)
+  OR   (subject = 'Literature' AND yr >= 2004)
+
+--#11
+/*
+Find all details of the prize won by PETER GRÜNBERG
+Non-ASCII characters
+*/
+SELECT * FROM nobel
+ WHERE winner = 'PETER GRÜNBERG'
+
+--#12
+/*
+Find all details of the prize won by EUGENE O'NEILL
+*/
+SELECT * FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+
+--#13
+/*
+List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+*/
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner;
+
+--#14
+/*
+Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+*/
