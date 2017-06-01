c3cb7f5d565b4cf0226520d8846b90ec21cd2f25
JoyceLee16 <stillsmile016@gmail.com>
2017-06-01 22:31:40 +0800

    Create SELECT from Nobel Tutorial.sql

---
+++ SELECT from Nobel Tutorial.sql
@@ -0,0 +1,12 @@
+--#1
+/*
+Show the name, continent and population of all countries.
+*/
+SELECT name, continent, population FROM world
+
+--#2
+/*
+Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+*/
+SELECT name FROM world
+WHERE population >= 200000000
