9b492715b993624f101ee1abcbce5c1f02d8ebc3
JoyceLee16 <stillsmile016@gmail.com>
2017-06-13 17:24:33 +0800

    Create Lesson6_JOIN and UEFA EURO.sql

--- Lesson6_JOIN and UEFA EURO.sql
+++ Lesson6_JOIN and UEFA EURO.sql
@@ -1,1 +1,109 @@
+--#1
+/*
+The first example shows the goal scored by a player with the last name 'Bender'. 
+The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
+Modify it to show the matchid and player name for all goals scored by Germany. 
+To identify German players, check for: teamid = 'GER'
+*/
+SELECT name, continent, population FROM world
 
+--#2
+/*
+From the previous query you can see that Lars Bender's scored a goal in game 1012. 
+Now we want to know what teams were playing in that match.
+Notice in the that the column matchid in the goal table corresponds to the id column in the game table. 
+We can look up information about game 1012 by finding that row in the game table.
+Show id, stadium, team1, team2 for just game 1012
+*/
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012
+
+--#3
+/*
+Give the name and the per capita GDP for those countries with a population of at least 200 million.
+#per capita GDP is the GDP divided by the population GDP/population
+*/
+SELECT name, GDP/population FROM world
+WHERE population >= 200000000
+
+--#4
+/*
+Show the name and population in millions for the countries of the continent 'South America'. 
+Divide the population by 1000000 to get population in millions.
+*/
+SELECT name,population/1000000 FROM world
+WHERE continent = 'South America'
+
+--#5
+/*
+Show the name and population for France, Germany, Italy
+*/
+SELECT name,population FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+--#6
+/*
+Show the countries which have a name that includes the word 'United'
+*/
+SELECT name FROM world
+WHERE name LIKE '%United%';
+
+--#7
+/*
+Show the countries that are big by area or big by population. Show name, population and area.
+*/
+SELECT name,population, area FROM world
+WHERE area > 3000000 OR population > 250000000;
+
+--#8
+/*
+Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+*/
+SELECT name, population, area FROM world
+WHERE area > 3000000 XOR population > 250000000;
+
+--#9
+/*
+Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. 
+Use the ROUND function to show the values to two decimal places.
+For South America show population in millions and GDP in billions both to 2 decimal places.
+*/
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2) FROM world
+WHERE continent = 'South America';
+
+--#10
+/*
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). 
+Round this value to the nearest 1000.
+*/
+SELECT name,  ROUND(gdp/population,-3) FROM world
+WHERE gdp > 1000000000000;
+
+--#11
+/*
+Show the name and capital where the name and the capital have the same number of characters.
+*/
+SELECT name, capital FROM world
+WHERE length(name) = length(capital);
+
+--#12
+/*
+Show the name and the capital where the first letters of each match.
+Don't include countries where the name and the capital are the same word.
+*/
+SELECT name,capital FROM world
+WHERE LEFT(name,1)=LEFT(capital,1) AND name<>capital
+
+--#13
+/*
+Find the country that has all the vowels and no spaces in its name.
+*/
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+   AND name LIKE '%e%'
+   AND name LIKE '%i%'
+   AND name LIKE '%o%'
+   AND name LIKE '%u%'
+   AND name NOT LIKE '% %'
